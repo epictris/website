8877052fae7ccdcd86e2f41df8037cf3196c63dd
tris <tris@p200300ee174ba01072074462543ab79d.dip0.t-ipconnect.de>
2025-03-02 20:38:17 +0100

    add database code

---
+++ app/backend/database/database.go
@@ -0,0 +1,90 @@
+package database
+
+import (
+	"context"
+	"database/sql"
+	"log"
+	"time"
+
+	"modernc.org/sqlite"
+)
+
+type DB struct {
+	dbPath string
+	Read *sql.DB
+	Write *sql.DB
+}
+
+const initSQL = `
+	PRAGMA journal_mode = WAL;
+	PRAGMA synchronous = NORMAL;
+	PRAGMA temp_store = MEMORY;
+	PRAGMA mmap_size = 30000000000; -- 30GB
+	PRAGMA busy_timeout = 5000;
+	PRAGMA automatic_index = true;
+	PRAGMA foreign_keys = ON;
+	PRAGMA analysis_limit = 1000;
+	PRAGMA trusted_schema = OFF;
+`
+
+func New(ctx context.Context, dbPath string) (db *DB, err error) {
+	db = &DB{
+		dbPath: dbPath,
+	}
+
+	// make sure every opened connection has the settings we expect
+	sqlite.RegisterConnectionHook(func(conn sqlite.ExecQuerierContext, _ string) error {
+		_, err = conn.ExecContext(ctx, initSQL, nil)
+
+		return err
+	})
+
+	write, err := sql.Open("sqlite", "file:"+db.dbPath)
+	if err != nil {
+		return
+	}
+	// only a single writer ever, no concurrency
+	write.SetMaxOpenConns(1)
+	write.SetConnMaxIdleTime(time.Minute)
+	if err != nil {
+		return
+	}
+
+	read, err := sql.Open("sqlite", "file:"+db.dbPath)
+	if err != nil {
+		return
+	}
+	// readers can be concurrent
+	read.SetMaxOpenConns(100)
+	read.SetConnMaxIdleTime(time.Minute)
+
+	db.Read = read
+	db.Write = write
+
+	return
+}
+
+const createTableSQL = `
+	CREATE TABLE IF NOT EXISTS users (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		google_id TEXT NOT NULL UNIQUE,
+		clipboard TEXT NOT NULL DEFAULT ''
+	);
+
+	DROP TABLE SESSIONS;
+	
+	CREATE TABLE IF NOT EXISTS sessions (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		user_id INTEGER NOT NULL,
+		token TEXT NOT NULL,
+		created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+		FOREIGN KEY(user_id) REFERENCES users(id)
+	);
+`
+
+func Init(db *DB) {
+	_, err := db.Write.Exec(createTableSQL)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	cloud.google.com/go/auth/oauth2adapt v0.2.7 // indirect
 	cloud.google.com/go/compute/metadata v0.6.0 // indirect
 	github.com/chmike/securecookie v1.3.5 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
@@ -14,12 +15,16 @@ require (
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.3.4 // indirect
 	github.com/googleapis/gax-go/v2 v2.14.1 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/ncruces/go-strftime v0.1.9 // indirect
+	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
 	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.59.0 // indirect
 	go.opentelemetry.io/otel v1.34.0 // indirect
 	go.opentelemetry.io/otel/metric v1.34.0 // indirect
 	go.opentelemetry.io/otel/trace v1.34.0 // indirect
 	golang.org/x/crypto v0.33.0 // indirect
+	golang.org/x/exp v0.0.0-20230315142452-642cacee5cc0 // indirect
 	golang.org/x/net v0.35.0 // indirect
 	golang.org/x/oauth2 v0.27.0 // indirect
 	golang.org/x/sys v0.30.0 // indirect
@@ -28,4 +33,8 @@ require (
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250219182151-9fdb1cabc7b2 // indirect
 	google.golang.org/grpc v1.70.0 // indirect
 	google.golang.org/protobuf v1.36.5 // indirect
+	modernc.org/libc v1.61.13 // indirect
+	modernc.org/mathutil v1.7.1 // indirect
+	modernc.org/memory v1.8.2 // indirect
+	modernc.org/sqlite v1.36.0 // indirect
 )
