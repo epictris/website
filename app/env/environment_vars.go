f28d2283376a9f7e38efd1a28effe7ab736aa34c
tris <tris@p200300ee174ba01072074462543ab79d.dip0.t-ipconnect.de>
2025-03-02 20:38:39 +0100

    add env var file

---
+++ app/env/environment_vars.go
@@ -0,0 +1,15 @@
+package env
+
+import (
+	"os"
+)
+
+func getEnvWithDefault(key string, defaultValue string) string {
+	value, exists := os.LookupEnv(key)
+	if !exists {
+		return defaultValue
+	}
+	return value
+}
+
+var DATA_DIR = getEnvWithDefault("DATA_DIR", "/data")
