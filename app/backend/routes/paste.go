c82b989e55db296b708afb0bfbacc5960f28a512
tris <tris@p200300ee174ba0603b5b6dac22b5c8e7.dip0.t-ipconnect.de>
2025-03-04 13:59:06 +0100

    remove modtime from content serve call

--- app/backend/routes/paste.go
+++ app/backend/routes/paste.go
@@ -1,11 +1,16 @@
 package routes
 
 import (
+	"fmt"
 	"net/http"
 	"os"
+	"time"
+
 	"tris.sh/project/app/backend/database"
 )
 func Paste(w http.ResponseWriter, r *http.Request, db *database.DB, user_id int) {
+	fmt.Println("user id:")
+	fmt.Println(user_id)
 	const file_path = "app/static/paste.html"
 	file, err := os.Open(file_path)
 	if err != nil {
@@ -18,5 +23,5 @@ func Paste(w http.ResponseWriter, r *http.Request, db *database.DB, user_id int)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	http.ServeContent(w, r, fileInfo.Name(), fileInfo.ModTime(), file)
+	http.ServeContent(w, r, fileInfo.Name(), time.Unix(0, 0), file)
 }
